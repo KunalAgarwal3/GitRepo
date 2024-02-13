879f6004fa21632c103e80dde5a475689b5a6aab
KunalAgarwal3 <kunaladarawal302@gmail.com>
2024-02-13 11:44:19 +0530

    Delete Syso

--- Cucumberjava/src/TestClass.java
+++ Cucumberjava/src/TestClass.java
@@ -2,8 +2,7 @@
 public class TestClass {
 public static void main(String[] args) {
 	System.out.println("Hello GitRepo");
-//	System.out.println("Hello");
-//	System.out.println("Hello java");
+
 	
 }
 }
