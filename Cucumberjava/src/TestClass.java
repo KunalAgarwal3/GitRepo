15be726c89a7f018bdb87f757387c34557d5b28b
KunalAgarwal3 <kunaladarawal302@gmail.com>
2024-02-13 17:42:38 +0530

    Update Syso

--- Cucumberjava/src/TestClass.java
+++ Cucumberjava/src/TestClass.java
@@ -3,6 +3,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello GitRepo");
 		System.out.println("adding Repo");
+		System.out.println("Hello ABCD");
 
 	}
 }
